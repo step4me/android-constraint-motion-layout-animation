faa19c25e28a1727cffb7b32482805af1f15c8e2
step4me <prosikeffect@gmail.com>
2018-07-31 11:54:04 +0900

    추가: 실습에서 사용할 기본 레이아웃

--- constraintlayout/src/main/java/com/step4me/constraintandmotionlayout/MainActivity.java
+++ constraintlayout/src/main/java/com/step4me/constraintandmotionlayout/MainActivity.java
@@ -2,6 +2,7 @@ package com.step4me.constraintandmotionlayout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onApplyClick(View view) {
+
+    }
+
+    public void onResetClick(View view) {
+
     }
 }
